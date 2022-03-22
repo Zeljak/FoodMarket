0764f7e2994aa4802f80366d24a613ef8e92ad69
badu22 <marko.badurina@gmail.com>
2022-03-22 15:10:27 +0100

    page scroll

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,29 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+
+setTimeout(function() {
+	window.scrollTo(0, 0);
+}, 1);
+
+$(function() {
+	window.scrollTo(0, 0);
+	
+	// variables
+	const hamburger = document.querySelector('.hamburger');
+	const navMenu = document.querySelector('.nav-menu');
+	const adress = window.location;
+
+	
+	// funcions
+	const mobileMenu = function () {
+		hamburger.classList.toggle('active');
+		navMenu.classList.toggle('active');
+	};
+
+	const pageScroll = location => {
+		$('html, body').animate(
+			{
+				scrollTop: $(location).offset().top
+			},
+			1000
+		);
+	};
+
+	// events
+	hamburger.addEventListener('click', mobileMenu);
+
+	$('.nav-link').on('click', event => {
+		const classArr = event.target.classList.value;
+		const hash = event.target.hash;
+		if (classArr.includes('scroll-me') && hash) {
+			event.preventDefault();
+			pageScroll(hash);
+		} else if (classArr.includes('nav-link--current')) {
+			event.preventDefault();
+			pageScroll('#top');
+		}
+		mobileMenu();
+	});
+
+	// on load action
+	if (adress.hash) {
+		pageScroll(adress.hash);
+
+		history.pushState('', document.title, window.location.pathname + window.location.search);
+	}
 });
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
 
 
-// Smooth Scrolling
-$(".nav-link").on("click", function(event){
-  if (this.hash !== "") {
-      event.preventDefault();
-      const hash = this.hash;
-      $("html, body").animate(
-          {
-              scrollTop: $(hash).offset().top
-          },
-          1800
-      );
-  }
-});
 
